581247a8096fc704b76ed131ad1f8df71be3259e
Harry de Boer <hldeboer@gmail.com>
2021-12-08 17:15:30 +0100

    added app:import command for initial data

--- README.md
+++ README.md
@@ -4,15 +4,15 @@ My Life
 <h3>Install</h3>
 <ol>
 <li>Copy .env.example to .env and fill in the fields.</li>
-<li>Build the docker-compose image and up -d the image</li>
-<li>Run composer install</li>
-<li>Run the migrations</li>
+<li>Build the docker-compose image and up -d the image.</li>
+<li>Run composer install.</li>
+<li>Run the migrations.</li>
 <li>Register the user in the route /register.</li>
 <li>Run npm install (not in container but with a global node install). 
 Then link the UglifyJS and SCSS File Watchers to the node_modules/.bin binaries. 
-The SCSS Arguments in PhpStorm is: --style compressed $FileParentDir$/scss/$FileName$:$FileParentDir$/css/$FileNameWithoutExtension$.css</li>
-<li>Optional: import the sql-files in src/WineBundle/Data and copy the labels to 
-public/img/labels.</li>
+The SCSS Arguments in PhpStorm is '--style compressed $FileParentDir$/scss/$FileName$:$FileParentDir$/css/$FileNameWithoutExtension$.css'.</li>
+<li>Run php bin/console app:import to fill the database with 
+initial data and have initial wine labels.</li>
 </ol>
 
 <h3>Testing</h3>
---
+++ src/Command/ImportCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Command;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ImportCommand extends Command
+{
+    protected static $defaultName = 'app:import';
+
+    public function __construct(
+            private EntityManagerInterface $em,
+            string $name = null,
+    )
+    {
+        parent::__construct($name);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $sqlFilesPath = dirname(__DIR__) . '/WineBundle/Data/sql-files';
+        $labelsPath = dirname(__DIR__) . '/WineBundle/Data/labels';
+        $publicLabelsPath = dirname(__DIR__,2) . '/public/img/labels';
+
+        $files = scandir($sqlFilesPath);
+        foreach($files as $file) {
+            if ($file !== '.' && $file !== '..') {
+                $this->em->getConnection()->executeQuery(file_get_contents($sqlFilesPath . '/' . $file));
+            }
+        }
+        echo 'SQL files loaded.' . PHP_EOL;
+
+        $files = scandir($labelsPath);
+        foreach($files as $file) {
+            if ($file !== '.' && $file !== '..') {
+                copy($labelsPath . '/' . $file, $publicLabelsPath . '/' . $file);
+            }
+        }
+        echo 'Labels moved to public/img/labels.' . PHP_EOL;
+
+        return Command::SUCCESS;
+    }
+}
