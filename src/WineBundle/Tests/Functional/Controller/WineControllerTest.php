5db72bbb36fd981e58b10fb1d79893fcc3dafba9
Harry de Boer <hldeboer@gmail.com>
2021-12-13 17:38:10 +0100

    used tick to check a checkbox in WineControllerTest

--- src/WineBundle/Tests/Functional/Controller/WineControllerTest.php
+++ src/WineBundle/Tests/Functional/Controller/WineControllerTest.php
@@ -15,8 +15,8 @@ class WineControllerTest extends AuthWebTestCase
     public function testCreateUpdateDelete(): void
     {
         $region = $this->getContainer()->get(RegionFactory::class)->create(['user' => $this->user]);
-        $grape0 = $this->getContainer()->get(GrapeFactory::class)->create(['user' => $this->user]);
-        $grape1 = $this->getContainer()->get(GrapeFactory::class)->create(['user' => $this->user]);
+        $this->getContainer()->get(GrapeFactory::class)->create(['user' => $this->user]);
+        $this->getContainer()->get(GrapeFactory::class)->create(['user' => $this->user]);
 
         $this->client->request('GET', '/wine');
 
@@ -32,8 +32,8 @@ class WineControllerTest extends AuthWebTestCase
         $testLabelPath = dirname(__DIR__) . '/test.png';
         $form['wine[label]'] = new File($testLabelPath);
         $form['wine[name]'] = 'test';
-        $form['wine[grapes]'][0]->setValue((string) $grape0->getId());
-        $form['wine[grapes]'][1]->setValue((string) $grape1->getId());
+        $form['wine[grapes]'][0]->tick();
+        $form['wine[grapes]'][1]->tick();
         $form['wine[year]'] = 2000;
         $form['wine[rating]'] = 7;
         $form['wine[price]'] = 10;
@@ -56,8 +56,6 @@ class WineControllerTest extends AuthWebTestCase
         $wineRepository = $this->getContainer()->get(WineRepositoryInterface::class);
 
         $wine = $wineRepository->findOneBy(['name' => 'test']);
-
-        $grapes = $wine->getGrapes();
 
         $this->client->request('GET', '/wine/single/' . $wine->getId());
 
