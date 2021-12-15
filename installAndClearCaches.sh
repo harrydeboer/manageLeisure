00d30f21511ea37939a10a0363c6738419668fec
Harry de Boer <hldeboer@gmail.com>
2021-12-15 19:06:58 +0100

    no interaction for live migrations

--- installAndClearCaches.sh
+++ installAndClearCaches.sh
@@ -4,7 +4,7 @@ PREFIX="docker exec -t manageLeisure"
 docker exec -t --user=www-data manageLeisure composer install --no-dev --no-progress --prefer-dist
 $PREFIX php bin/console cache:clear
 ./opcacheReset.sh
-until $PREFIX php bin/console doctrine:migrations:migrate
+until $PREFIX php bin/console doctrine:migrations:migrate --no-interaction
 do
   echo "Try again"
   sleep 1
