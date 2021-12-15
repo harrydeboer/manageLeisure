b2760b03ac50b1104449f1640f7580ae73e07407
Harry de Boer <hldeboer@gmail.com>
2021-12-15 22:36:09 +0100

    docker-compose.live.yml

--- bin/dockerBuildAndUp.sh
+++ bin/dockerBuildAndUp.sh
@@ -1,4 +1,4 @@
-docker-compose -f docker-compose.yml -f docker-compose.{APP_ENV}.yml build --no-cache
-docker-compose -f docker-compose.yml -f docker-compose.{APP_ENV}.yml up -d --remove-orphans
+docker-compose -f docker-compose.yml -f docker-compose.live.yml -f docker-compose.{APP_ENV}.yml build --no-cache
+docker-compose -f docker-compose.yml -f docker-compose.live.yml -f docker-compose.{APP_ENV}.yml up -d --remove-orphans
 docker cp /home/letsencrypt manageLeisure:/etc
-docker-compose -f docker-compose.yml -f docker-compose.{APP_ENV}.yml up -d --remove-orphans
+docker-compose -f docker-compose.yml -f docker-compose.live.yml -f docker-compose.{APP_ENV}.yml up -d --remove-orphans
