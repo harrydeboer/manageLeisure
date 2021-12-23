31f661ad120c60b12438568631b9a5fb6b047a2b
Harry de Boer <hldeboer@gmail.com>
2021-12-23 18:09:40 +0100

    set -e in build docker and up command

--- bin/dockerBuildAndUp.sh
+++ bin/dockerBuildAndUp.sh
@@ -1,3 +1,5 @@
+set -e
+
 docker-compose -f docker-compose.yml -f docker-compose.live.yml -f docker-compose.{APP_ENV}.yml build --no-cache
 docker-compose -f docker-compose.yml -f docker-compose.live.yml -f docker-compose.{APP_ENV}.yml up -d --remove-orphans
 docker cp /home/letsencrypt manageLeisure:/etc
