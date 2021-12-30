81ae6ac488ac6ab2b3aa52962262d5ac04a89e28
Harry de Boer <hldeboer@gmail.com>
2021-12-30 13:10:36 +0100

    git reset to previous head as www-data

--- rollback.sh
+++ rollback.sh
@@ -3,5 +3,5 @@ if [[ $EUID -eq 0 ]]; then
   echo "This script must NOT be run as root" 1>&2
   exit 1
 fi
-git reset --hard "HEAD@{1}"
+sudo -u www-data git reset --hard "HEAD@{1}"
 ./installAndClearCaches.sh
