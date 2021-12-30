49618aff9f00512d92d4a93e849fccdf02f0a4ae
Harry de Boer <hldeboer@gmail.com>
2021-12-30 13:23:44 +0100

    opcache reset as www-data

--- opcacheReset.sh
+++ opcacheReset.sh
@@ -3,7 +3,7 @@ PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" || exit ; pwd -P )
 PARENT_DIR="$(basename "$PARENT_PATH")"
 PUBLIC_DIR=${PARENT_PATH}/public/
 RANDOM_NAME=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 100).php
-echo "<?php opcache_reset(); echo 'OPcache reset!' . PHP_EOL; ?>" > "${PUBLIC_DIR}""${RANDOM_NAME}"
+sudo -u www-data echo "<?php opcache_reset(); echo 'OPcache reset!' . PHP_EOL; ?>" > "${PUBLIC_DIR}""${RANDOM_NAME}"
 docker cp "${PUBLIC_DIR}""${RANDOM_NAME}" manageLeisure:/var/www/html/public/"${RANDOM_NAME}"
 
 if [[ $PARENT_DIR = "staging.manageleisure.com" ]]; then
@@ -13,5 +13,5 @@ elif [[ $PARENT_DIR = "manageleisure.com" ]]; then
 elif [[ $PARENT_DIR = "manageLeisure" ]]; then
   curl http://manageleisure/"${RANDOM_NAME}"
 fi
-rm "${PUBLIC_DIR}""${RANDOM_NAME}"
+sudo -u www-data rm "${PUBLIC_DIR}""${RANDOM_NAME}"
 docker exec -it manageLeisure rm /var/www/html/public/"${RANDOM_NAME}"
