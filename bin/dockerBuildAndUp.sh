03e7f073eb53b76179e6b96c89431e98ab110f4f
Harry de Boer <hldeboer@gmail.com>
2021-12-31 11:31:37 +0100

    letsencrypt in /var/www

--- bin/dockerBuildAndUp.sh
+++ bin/dockerBuildAndUp.sh
@@ -3,5 +3,5 @@ set -e
 
 docker-compose -f docker-compose.yml -f docker-compose.live.yml -f docker-compose.{APP_ENV}.yml build --no-cache
 docker-compose -f docker-compose.yml -f docker-compose.live.yml -f docker-compose.{APP_ENV}.yml up -d --remove-orphans
-docker cp /home/letsencrypt manageLeisure:/etc
+docker cp /var/www/letsencrypt manageLeisure:/etc
 docker-compose -f docker-compose.yml -f docker-compose.live.yml -f docker-compose.{APP_ENV}.yml up -d --remove-orphans
