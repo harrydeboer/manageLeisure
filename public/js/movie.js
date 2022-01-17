5cee4b8e2429b412602b74cf23baf01ce8558e60
Harry de Boer <hldeboer@gmail.com>
2022-01-17 14:48:11 +0100

    document ready is deprecated and is removed in movie.js

--- public/js/movie.js
+++ public/js/movie.js
@@ -1,39 +1,37 @@
-$(document).ready(function () {
-    /** When there is no swearing then a request is send to the server, which sends the email. */
-    $('[name=movie]').on('submit', function (event) {
-        $('#hide-all').show();
+/** When there is no swearing then a request is sent to the server, which sends the email. */
+$('[name=movie]').on('submit', function (event) {
+    $('#hide-all').show();
 
-        $.ajax({
-            url: '/movie/get-rating',
-            type: 'GET',
-            data: $(this).serialize(),
-            success: function(data) {
-                $('#hide-all').hide();
-                $('#get-rating').html(data);
-            },
-            error: function(data) {
-                $('#hide-all').hide();
-                $('#get-rating').html(data.responseText);
-            }
-        });
-
-        event.preventDefault();
-    });
-
-    $('#get-rating').on('click', 'a', function(event) {
-        $('#hide-all').show();
-
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-
-        $.get($(this).attr('href'), '', function (data) {
+    $.ajax({
+        url: '/movie/get-rating',
+        type: 'GET',
+        data: $(this).serialize(),
+        success: function(data) {
             $('#hide-all').hide();
             $('#get-rating').html(data);
-        });
-
-        event.preventDefault();
+        },
+        error: function(data) {
+            $('#hide-all').hide();
+            $('#get-rating').html(data.responseText);
+        }
     });
+
+    event.preventDefault();
+});
+
+$('#get-rating').on('click', 'a', function(event) {
+    $('#hide-all').show();
+
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+
+    $.get($(this).attr('href'), '', function (data) {
+        $('#hide-all').hide();
+        $('#get-rating').html(data);
+    });
+
+    event.preventDefault();
 });
